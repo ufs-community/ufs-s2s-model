9a02c8f76ff4340f8f211c6f4d06e545d1cb13a5
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2020-04-15 21:19:25 +0000

    first commit: tests/rt.sh works for cpld_fv3_mom6_cice_cold_atm_flux

---
+++ tests/default_vars.sh
@@ -0,0 +1,245 @@
+
+###############################################################################
+#
+# Export variables to the default values
+#  - first common variables, then model specific ones
+#  - different machines, different defaults:
+#
+###############################################################################
+
+if [ $MACHINE_ID = wcoss ]; then
+
+  TASKS_dflt=144 ; TPN_dflt=16 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [ $MACHINE_ID = wcoss_cray ]; then
+
+  TASKS_dflt=150 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=24 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=24 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [ $MACHINE_ID = wcoss_dell_p3 ]; then
+
+  TASKS_dflt=150 ; TPN_dflt=28 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=14 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=28 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=28 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [[ $MACHINE_ID = hera.* ]]; then
+
+  TASKS_dflt=150 ; TPN_dflt=40 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=20 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [[ $MACHINE_ID = jet.* ]]; then
+
+  TASKS_dflt=150 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [[ $MACHINE_ID = gaea.* ]]; then
+
+  TASKS_dflt=150 ; TPN_dflt=36 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=18 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=18 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=18 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [[ $MACHINE_ID = cheyenne.* ]]; then
+
+  TASKS_dflt=150 ; TPN_dflt=36 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=18 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=18 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=18 ; INPES_strnest=2 ; JNPES_strnest=4
+
+elif [[ $MACHINE_ID = stampede.* ]]; then
+
+  TASKS_dflt=150 ; TPN_dflt=48 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=24 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
+
+else
+
+  echo "Unknown MACHINE_ID ${MACHINE_ID}"
+  exit 1
+
+fi
+
+# Re-instantiate COMPILER in case it gets deleted by module purge
+COMPILER=${NEMS_COMPILER:-intel}
+# Longer default walltime for GNU and PGI
+if [[ $COMPILER = gnu ]] || [[ $COMPILER = pgi ]]; then
+    WLCLK_dflt=30
+else
+    WLCLK_dflt=15
+fi
+
+export_fv3 ()
+{
+export THRD=1
+export WLCLK=$WLCLK_dflt
+export INPES=$INPES_dflt
+export JNPES=$JNPES_dflt
+export TASKS=$TASKS_dflt
+export TPN=$TPN_dflt
+export RESTART_INTERVAL=0
+export QUILTING=.true.
+export WRITE_GROUP=1
+export WRTTASK_PER_GROUP=6
+export OUTPUT_HISTORY=.true.
+export WRITE_DOPOST=.false.
+export NUM_FILES=2
+export FILENAME_BASE="'dyn' 'phy'"
+export OUTPUT_GRID="'cubed_sphere_grid'"
+export OUTPUT_FILE="'netcdf'"
+export WRITE_NEMSIOFLIP=.false.
+export WRITE_FSYNCFLAG=.false.
+export IMO=384
+export JMO=190
+
+# Coldstart/warmstart
+export WARM_START=.F.
+export READ_INCREMENT=.F.
+export NGGPS_IC=.T.
+export EXTERNAL_IC=.T.
+export MAKE_NH=.T.
+export MOUNTAIN=.F.
+export NA_INIT=1
+
+# Microphysics
+export IMP_PHYSICS=11
+# GFDL MP
+export DNATS=1
+export DO_SAT_ADJ=.T.
+export LHEATSTRG=.F.
+export LGFDLMPRAD=.F.
+export EFFR_IN=.F.
+# Thompson MP
+export LRADAR=.T.
+export LTAEROSOL=.T.
+
+# GWD
+export LDIAG_UGWP=.F.
+export DO_UGWP=.F.
+export DO_TOFD=.F.
+export GWD_OPT=1
+
+# PBL
+export SATMEDMF=.F.
+export ISATMEDMF=0
+export HYBEDMF=.T.
+export SHINHONG=.F.
+export DO_YSU=.F.
+export DO_MYNNEDMF=.F.
+export DO_MYJPBL=.F.
+
+# Shallow/deep convection
+export IMFSHALCNV=2
+export IMFDEEPCNV=2
+
+# SFC
+export DO_MYJSFC=.F.
+
+# LSM
+export LSM=1
+export LSOIL_LSM=4
+export LANDICE=.T.
+
+# Ozone / stratospheric H2O
+export OZ_PHYS_OLD=.T.
+export OZ_PHYS_NEW=.F.
+export H2O_PHYS=.F.
+
+export CPL=.F. #TODO fv3_defaults use CPL=.false. check equivalency.
+export CPLFLX=.F.
+export CPLWAV=.F.
+export DAYS=1
+export NPX=97
+export NPY=97
+export NPZ=64
+export NPZP=65
+export NSTF_NAME=2,1,1,0,5
+export FDIAG=0,1,2,3,4,5,6,7,8,9,10,11,12,15,18,21,24
+export NFHOUT=3
+export NFHMAX_HF=12
+export NFHOUT_HF=1
+export FNALBC="'global_snowfree_albedo.bosu.t126.384.190.rg.grb',"
+export FNVETC="'global_vegtype.igbp.t126.384.190.rg.grb',"
+export FNSOTC="'global_soiltype.statsgo.t126.384.190.rg.grb',"
+export FNSMCC="'global_soilmgldas.t126.384.190.grb',"
+export FNABSC="'global_mxsnoalb.uariz.t126.384.190.rg.grb',"
+
+
+export ENS_NUM=1
+export SYEAR=2016
+export SMONTH=10
+export SDAY=03
+export SHOUR=00
+export FHMAX=${FHMAX:-`expr $DAYS \* 24`}
+export DT_ATMOS=1800
+export FHCYC=24
+
+# Stochastic physics
+export DO_SPPT=.F.
+export DO_SHUM=.F.
+export DO_SKEB=.F.
+export DO_SFCPERTS=.F.
+export SKEB=-999.
+export SPPT=-999.
+export SHUM=-999.
+
+#IAU
+export IAU_INC_FILES="''"
+export IAU_DRYMASSFIXER=.false.
+}
+
+export_cpl ()
+{ 
+export TASKS=318
+export ATMRES=''
+export OCNRES='025'
+export INPUT_NML="input.mom6.nml.IN"
+export FIELD_TABLE="field_table"
+export NSOUT='-1'
+export CPLFLX='.T.'
+export CPL='.true.'
+export NSTF_NAME='0,0,0,0,0'
+export MOM6_RESTART_SETTING='n'
+export med_model="nems"
+export atm_model="fv3"
+export ocn_model="mom6"
+export ice_model="cice"
+# future use: for MOM6 river runoff 
+export MOM6_RIVER_RUNOFF='True'
+export NPROC_ICE='48'
+export DT_ATMOS='900' #needed for C96 cases
+export DT_DYNAM_MOM6='900'
+export DT_THERM_MOM6='1800'
+# defaults for CICE runtype and restart writing
+export RUNTYPE='initial' 
+export DUMPFREQ='d' 
+export DUMPFREQ_N='35' 
+export USE_RESTART_TIME='.false.'
+# setting to true will allow Frazil FW and Salt to be
+# included in fluxes sent to ocean
+export FRAZIL_FWSALT='.true.'
+# default to write CICE average history files
+export CICE_HIST_AVG='.true.'
+# default setting for runid
+export RUNID='unknown'
+# for FV3: default values will be changed if doing a warm-warm restart
+export WARM_START='.F.'
+export MAKE_NH='.T.'
+export NA_INIT='1'
+export EXTERNAL_IC='.T.'
+export NGGPS_IC='.T.'
+export MOUNTAIN='.F.'
+
+export med_petlist_bounds="0 143"
+export atm_petlist_bounds="0 149"
+export ocn_petlist_bounds="150 269"
+export ice_petlist_bounds="270 317"
+}
---
+++ tests/rt_fv3.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -eux
+
+source rt_utils.sh
+source atparse.bash
+source edit_inputs.sh
+
+mkdir -p ${RUNDIR}
+cd $RUNDIR
+
+###############################################################################
+# Make configure and run files
+###############################################################################
+
+# FV3 executable:
+cp ${PATHRT}/$FV3X                                 fv3_mom6_cice.exe
+
+# modulefile for FV3 prerequisites:
+cp ${PATHRT}/modules.fv3_mom6_cice_${COMPILE_NR}             modules.fv3
+
+# Get the shell file that loads the "module" command and purges modules:
+cp ${PATHRT}/../NEMS/src/conf/module-setup.sh.inc  module-setup.sh
+#cp ${PATHTR}/parm/post_itag itag
+#cp ${PATHTR}/parm/postxconfig-NT.txt postxconfig-NT.txt
+#cp ${PATHTR}/parm/postxconfig-NT_FH00.txt postxconfig-NT_FH00.txt
+#cp ${PATHTR}/parm/params_grib2_tbl_new params_grib2_tbl_new
+
+SRCD="${PATHTR}"
+RUND="${RUNDIR}"
+
+# Set up the run directory
+atparse < ${PATHRT}/fv3_conf/${FV3_RUN:-fv3_run.IN} > fv3_run
+source ./fv3_run
+atparse < ${PATHTR}/parm/${INPUT_NML:-input.nml.IN} > input.nml
+atparse < ${PATHTR}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+
+edit_ice_in < ${PATHTR}/parm/ice_in_template > ice_in
+edit_mom_input < ${PATHTR}/parm/MOM_input_template > INPUT/MOM_input
+edit_diag_table < ${PATHTR}/parm/diag_table_template > diag_table
+cp ${PATHTR}/parm/data_table data_table
+
+if [[ "Q${INPUT_NEST02_NML:-}" != Q ]] ; then
+    atparse < ${PATHTR}/parm/${INPUT_NEST02_NML} > input_nest02.nml
+fi
+
+if [[ $SCHEDULER = 'moab' ]]; then
+  atparse < $PATHRT/fv3_conf/fv3_msub.IN > job_card
+elif [[ $SCHEDULER = 'pbs' ]]; then
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_qsub.IN > job_card
+elif [[ $SCHEDULER = 'sbatch' ]]; then
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_qsub.IN > job_card
+elif [[ $SCHEDULER = 'slurm' ]]; then
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_slurm.IN > job_card
+elif [[ $SCHEDULER = 'lsf' ]]; then
+  if (( TASKS < TPN )); then
+    TPN=${TASKS}
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_bsub.IN > job_card
+fi
+
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+
+################################################################################
+# Submit test
+################################################################################
+
+if [[ $ROCOTO = 'false' ]]; then
+  submit_and_wait job_card
+else
+  chmod u+x job_card
+  ./job_card
+fi
+
+check_results
+
+################################################################################
+# End test
+################################################################################
+
+exit 0
