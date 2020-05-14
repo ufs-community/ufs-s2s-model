4fbe93b136aed2e5123567c55c50eefa7906cc5b
Shrinivas.Moorthi <Shrinivas.Moorthi@noaa.gov>
2020-05-14 13:50:18 +0000

    fixing missed logic in before_components.mk and adding fv3_debug modulefiles in dell and hera

--- conf/before_components.mk
+++ conf/before_components.mk
@@ -1,27 +1,59 @@
-# This file sets the location of configure.nems and modules.nems. 
+# This file sets the location of configure.nems and modules.nems, and
+# adds Make rules to create the tests/*.exe and tests/modules.* files.
 # This file is included by the NEMS build system, within
 # NEMS/GNUmakefile, just after platform logic is executed, but before
 # the appbuilder file (if any) is read.
 
 # IMPORTANT: This file MUST NOT CONTAIN any logic specific to building
 # FV3, CCPP, FMS, MOM6, CICE, or NEMS.  Otherwise, applications will
-# break.  There should only be logic specific this coupled app. 
+# break.  There should only be logic specific this ufs app. 
 #
-# Logic specific to FV3, CCPP, FMS, or NEMS belong in NEMS/src/incmake.
+# Logic specific to FV3, CCPP, FMS, WW3, or NEMS belong in NEMS/src/incmake.
 
 # ----------------------------------------------------------------------
 # Decide the conf and modulefile names.
+# S2S_DEBUG_MODULE is defined in GNUmakefile
 
 CHOSEN_MODULE=$(BUILD_TARGET)/fv3
+
+ifeq ($(S2S_DEBUG_MODULE),true)
+  CHOSEN_MODULE=$(CHOSEN_MODULE)_debug
+# CHOSEN_MODULE=$(BUILD_TARGET)/fv3_debug
+endif
+
+#$(info CHOSEN_MODULE is $(CHOSEN_MODULE))
+
+ifneq (,$(findstring INTEL16=Y,$(FV3_MAKEOPT)))
+  ifeq ($(CHOSEN_MODULE),gaea.intel/fv3)
+    override CHOSEN_MODULE=$(BUILD_TARGET)/fv3.intel-16.0.3.210
+    $(warning Overriding CHOSEN_MODULE with $(CHOSEN_MODULE) as requested per MAKEOPT)
+  endif
+endif
 
 CONFIGURE_NEMS_FILE=configure.fv3.$(BUILD_TARGET)
 
 # ----------------------------------------------------------------------
 # Exit for systems that are currently not supported
-ifeq ($(BUILD_TARGET),gaea.intel)
-  $(error App currently not supported on $(BUILD_TARGET))
-else ifeq ($(BUILD_TARGET),jet.intel)
-  $(error App currently not supported on $(BUILD_TARGET))
+
+ifeq ($(BUILD_TARGET),theia.pgi)
+  $(error NEMSfv3gfs currently not supported on $(BUILD_TARGET))
 else ifeq ($(BUILD_TARGET),cheyenne.pgi)
-  $(error App currently not supported on $(BUILD_TARGET))
+  $(error NEMSfv3gfs currently not supported on $(BUILD_TARGET))
+endif
+
+
+# ----------------------------------------------------------------------
+# Copy the executable and modules.nems files into the tests/ directory
+# if a TEST_BUILD_NAME is specified.
+ 
+ifneq ($(TEST_BUILD_NAME),)
+ $(info Will copy modules.nems and NEMS.x as $(TEST_BUILD_NAME) under tests/)
+ $(ROOTDIR)/tests/$(TEST_BUILD_NAME).exe: $(NEMS_EXE)
+	set -xe ; cp "$<" "$@"
+ 
+ $(ROOTDIR)/tests/modules.$(TEST_BUILD_NAME): $(NEMSDIR)/src/conf/modules.nems
+	set -xe ; cp "$<" "$@"
+ 
+ configure: $(ROOTDIR)/tests/modules.$(TEST_BUILD_NAME) ;
+ build: $(ROOTDIR)/tests/$(TEST_BUILD_NAME).exe ;
 endif
