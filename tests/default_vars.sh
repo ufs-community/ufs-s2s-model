20a389b30d064422567bd7cf22df0bdeb667a158
denise.worthen <Denise.Worthen@noaa.gov>
2020-08-06 11:25:45 -0400

    update parm w/ cap debug flags

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -308,6 +308,7 @@ export atm_model="fv3"
 export ocn_model="mom6"
 export ice_model="cice6"
 export wav_model="ww3"
+export cap_dbug_flag="0"
 # MOM6 river runoff 
 export MOM6_RIVER_RUNOFF='True'
 # set USE_LA_LI2016 to the current default; this must be set False for restart repro 
