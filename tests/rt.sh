c89a04f6e383073a156b9369bfaa643b26c55125
denise.worthen <Denise.Worthen@noaa.gov>
2020-08-07 15:14:49 -0500

    specify special baseline area for cice6 tests

--- tests/rt.sh
+++ tests/rt.sh
@@ -436,7 +436,8 @@ if [[ $MACHINE_ID = cheyenne.* ]]; then
   RTPWD=${RTPWD:-$DISKNM/develop-20200210/${COMPILER^^}}
 else
   #RTPWD=${RTPWD:-$DISKNM/FV3-MOM6-CICE5/develop-20200718}
-  RTPWD="/scratch1/NCEPDEV/stmp4/Denise.Worthen/cice6_baselines"
+  # for 35d tests of cice6 on orion
+  RTPWD="/work/noaa/stmp/dworthen/stmp/dworthen/cice6_baselines"
 fi
 
 shift $((OPTIND-1))
