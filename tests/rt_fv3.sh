06755639351765014dffd72135adb445f4c055d3
Brian Curtis <brian.curtis@noaa.gov>
2020-10-05 13:13:39 -0500

    Move parm/ into tests/ (Closes: #208)

--- tests/rt_fv3.sh
+++ tests/rt_fv3.sh
@@ -20,10 +20,10 @@ cp ${PATHRT}/modules.fcst_${COMPILE_NR}             modules.fcst
 
 # Get the shell file that loads the "module" command and purges modules:
 cp ${PATHRT}/../NEMS/src/conf/module-setup.sh.inc  module-setup.sh
-#cp ${PATHTR}/parm/post_itag itag
-#cp ${PATHTR}/parm/postxconfig-NT.txt postxconfig-NT.txt
-#cp ${PATHTR}/parm/postxconfig-NT_FH00.txt postxconfig-NT_FH00.txt
-#cp ${PATHTR}/parm/params_grib2_tbl_new params_grib2_tbl_new
+#cp ${PATHRT}/parm/post_itag itag
+#cp ${PATHRT}/parm/postxconfig-NT.txt postxconfig-NT.txt
+#cp ${PATHRT}/parm/postxconfig-NT_FH00.txt postxconfig-NT_FH00.txt
+#cp ${PATHRT}/parm/params_grib2_tbl_new params_grib2_tbl_new
 
 SRCD="${PATHTR}"
 RUND="${RUNDIR}"
@@ -31,21 +31,21 @@ RUND="${RUNDIR}"
 # Set up the run directory
 atparse < ${PATHRT}/fv3_conf/${FV3_RUN:-fv3_run.IN} > fv3_run
 source ./fv3_run
-atparse < ${PATHTR}/parm/${INPUT_NML:-input.nml.IN} > input.nml
-atparse < ${PATHTR}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
-atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+atparse < ${PATHRT}/parm/${INPUT_NML:-input.nml.IN} > input.nml
+atparse < ${PATHRT}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
+atparse < ${PATHRT}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
 
-edit_ice_in < ${PATHTR}/parm/ice_in_template > ice_in
-edit_mom_input < ${PATHTR}/parm/${MOM_INPUT:-MOM_input_template_$OCNRES} > INPUT/MOM_input
-edit_diag_table < ${PATHTR}/parm/diag_table_template > diag_table
-edit_data_table < ${PATHTR}/parm/data_table_template > data_table
+edit_ice_in < ${PATHRT}/parm/ice_in_template > ice_in
+edit_mom_input < ${PATHRT}/parm/${MOM_INPUT:-MOM_input_template_$OCNRES} > INPUT/MOM_input
+edit_diag_table < ${PATHRT}/parm/diag_table_template > diag_table
+edit_data_table < ${PATHRT}/parm/data_table_template > data_table
 # CMEPS 
-cp ${PATHTR}/parm/fd_nems.yaml fd_nems.yaml
-cp ${PATHTR}/parm/pio_in pio_in
-cp ${PATHTR}/parm/med_modelio.nml med_modelio.nml
+cp ${PATHRT}/parm/fd_nems.yaml fd_nems.yaml
+cp ${PATHRT}/parm/pio_in pio_in
+cp ${PATHRT}/parm/med_modelio.nml med_modelio.nml
 
 if [[ "Q${INPUT_NEST02_NML:-}" != Q ]] ; then
-    atparse < ${PATHTR}/parm/${INPUT_NEST02_NML} > input_nest02.nml
+    atparse < ${PATHRT}/parm/${INPUT_NEST02_NML} > input_nest02.nml
 fi
 
 if [[ $SCHEDULER = 'moab' ]]; then
@@ -75,7 +75,7 @@ elif [[ $SCHEDULER = 'lsf' ]]; then
   atparse < $PATHRT/fv3_conf/fv3_bsub.IN > job_card
 fi
 
-atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+atparse < ${PATHRT}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
 
 ################################################################################
 # Submit test
